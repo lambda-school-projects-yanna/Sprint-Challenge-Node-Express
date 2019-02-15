372e0fd63d56d42f6ad523e5768c8cea047a7e4e
yannafaith <yannafaith97@gmail.com>
2019-02-15 11:42:05 -0700

    react app shows projects from api

--- test/src/components/Projects.js
+++ test/src/components/Projects.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 class Projects extends React.Component {
     state = {
         projects: null
     }
 
+
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:4000/api/projects/')
+            .then((res) => {
+                this.setState({projects: res.data})
+                console.log(this.state.projects);
+            })
+            .catch(err => console.log(err))
+    }
+
+
     render() {
         return (
             <div>
                 <h2>Projects</h2>
+                { this.state.projects && this.state.projects.map(project => {
+                    return (
+                        <div>
+                           <h3>{project.name}</h3> 
+                           <p>{project.description}</p> 
+                        </div>
+                    );
+                })}
             </div>
         );
     }
